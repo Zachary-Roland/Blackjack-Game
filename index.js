842bf415f5ca019586d74aa73cef6894bc4ef64d
Zachary-Roland <zsolo95@gmail.com>
2021-07-22 11:26:19 -0500

    changes build directory location to blackjack.html

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8889;
 
 app.use(express.json());
 app.use(express.static(__dirname + "/build"));
 
 app.get("*", (req, res) => {
-  return res.sendFile("/build/index.html", { root: __dirname + "/" });
+  return res.sendFile("/build/blackjack.html", { root: __dirname + "/" });
 });
 
 app.listen(PORT, () => console.log("Well hello there!"));
